e3fd64ff11dd1ec7624d8455b197fa87c08462f1
Ivan Volkov <volkivanv@mail.ru>
2023-09-04 21:28:25 +0300

    Добавили задачу 3

---
+++ Seminar1Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input WeekDay");
+int day = int.Parse(Console.ReadLine()??"0");
+string[] dayOfWeek = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+if (day > 0 && day < 8)
+{
+    Console.WriteLine("День недели "+ dayOfWeek[day-1]);
+} else {
+    Console.WriteLine("Неверный ввод");
+}
+
