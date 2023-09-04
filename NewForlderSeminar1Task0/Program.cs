9c223d236d6104b66ce37b5604a5ae1171213c72
Ivan Volkov <volkivanv@mail.ru>
2023-09-04 20:50:55 +0300

    Написали программу Квадрат числа

--- NewForlderSeminar1Task0/Program.cs
+++ NewForlderSeminar1Task0/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача №0//
+//Напишите программу, которая на вход принимает 
+//число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Введите число:");
+string? inputNum = Console.ReadLine();//??'0';
+if(inputNum!=null)
+{ 
+    //Парсим введенное число
+    int num = int.Parse(inputNum);
+    int res = num*num;
+    Console.WriteLine("Result is " + res);
+}
+
+Console.WriteLine("Квадрат числа:" + (int)Math.Pow(int.Parse(inputNum), 2));
