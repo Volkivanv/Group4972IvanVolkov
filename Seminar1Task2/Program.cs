9798a99ff90d6e6b29e8a3c3e1a35ec626312707
Ivan Volkov <volkivanv@mail.ru>
2023-09-05 21:26:31 +0300

    Сделано ДЗ

---
+++ Seminar1Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input a!");
+double a = Double.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Input b!");
+double b = Double.Parse(Console.ReadLine()??"0");
+if (a < b){ 
+    Console.WriteLine("Max number is b = " + b);
+}else{
+    Console.WriteLine("Max number is b = " + a);
+};
---
+++ Seminar1Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Input first number!");
+double a = double.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Input second number!");
+double b = double.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Input third number!");
+double c = double.Parse(Console.ReadLine()??"0");
+
+double max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max) 
+{
+    max = c;
+}
+
+Console.WriteLine("Maximum is " + max);
