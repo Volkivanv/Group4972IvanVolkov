68b876584ecf6f1769365f2a5c6971a751689224
Ivan Volkov <volkivanv@mail.ru>
2023-09-08 21:08:59 +0300

    Сделано 2-ое домашнее задание

---
+++ Seminar2Task13/Program.cs
@@ -0,0 +1,34 @@
+﻿// №13 Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// * Сделать вариант для числа длиной до 10 цифр не используя char или string
+
+int numberLength(long number) //функция для определения длины числа
+{
+    int length = 0;
+    while (number > 0)
+    {
+        length++;
+        number /= 10;
+    }
+    return length;
+}
+
+Console.WriteLine("введите число");
+long n = long.Parse(Console.ReadLine() ?? "0");
+int digitNumber = 3; // какую цифру с начала нужно отобразить
+if (n >= Convert.ToInt32(Math.Pow(10, digitNumber - 1))) // проверка что количество 
+                                                         //знаков хватит для отображения
+{
+    //Console.WriteLine(numberLength(n) + " это длина " + n);
+    //отбрасываем последние знаки числа, так чтобы осталось digitNumber знаков
+    long tempN = n / Convert.ToInt32((Math.Pow(10, numberLength(n) - (digitNumber))));
+    Console.WriteLine(digitNumber + " цифра с начала " + tempN % 10); //выводим последнюю цифру оставшегося
+}
+else // если количество знаков меньше нужного, выводим сообщение пользователю об этом;
+{
+    Console.WriteLine("Ваше число имеет меньше " + digitNumber + " цифр");
+}
+
---
+++ Seminar2task15/Program.cs
@@ -0,0 +1,29 @@
+﻿// №15 Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//  и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+// * Сделать вариант с использованием конструкции Dictionary
+
+string DayOfWeek(int dayNum)
+{ // метод, возвращающий день недели по номеру
+    return
+    System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName
+    ((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(dayNum % 7));
+}
+
+bool suit = false; // определяем флаг выхода
+while (!suit)
+{ // 
+    Console.WriteLine("введите номер дня недели от 1 до 7");
+    int inDayNum = int.Parse(Console.ReadLine() ?? "0"); //вводим день недели
+    suit = 0 <= inDayNum && inDayNum <= 7; //попадает ли день недели в нужный диапазон
+    if (suit)
+    { // если попадает то выводим его название
+        Console.WriteLine(inDayNum + " день недели " + DayOfWeek(inDayNum));
+    }
+    else
+    { // если нет, то сообщаем пользователю о неправильном вводе
+        Console.WriteLine("День недели введен неправильно. Введите заново номер дня недели от 1 до 7");
+    }
+}
