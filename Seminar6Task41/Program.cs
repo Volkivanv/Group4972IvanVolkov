86a7656f2433d62cf45b39bc3b0fe5d9d69e8dd5
Ivan Volkov <volkivanv@mail.ru>
2023-09-25 01:21:52 +0300

    Делаю ДЗ

---
+++ Seminar6Task41/Program.cs
@@ -0,0 +1,52 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+// (Задание со звездочкой) Пользователь вводит число нажатий,
+//  затем программа следит за нажатиями и выдает сколько чисел больше 0 было введено.
+
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+string InputExpression(string msg) //ввод строки
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine()??"0";
+}
+
+double[] ParseExpression(string str) // парсинг строки на числа с помощью регулярного выражения
+{
+    double[] result = Regex.Matches(str, @"([-+]?\d+(?:\.\d+)?)")
+    .OfType<Match>()
+    .Select(m => double.Parse(m.Groups[1].Value, 
+        CultureInfo.GetCultureInfo("en-EN")) * (m.Groups[1].Value.StartsWith("0.0") ? 10 : 1))
+    .ToArray();
+
+    return result;
+}
+
+int numberPositive(double[] arr) //подсчет положительных чисел
+{
+    int posNum = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >=0) 
+            posNum++;
+    }
+    return posNum;
+}
+
+
+
+void OutPutArray(double[] arr) // вывод массива
+{
+    Console.WriteLine($"{String.Join("; ", arr)}");
+}
+
+
+string expression = InputExpression("Введите строку. "+ 
+"Для чисел, в качестве десятичного разделителя применяйте точку!");
+double[] values = ParseExpression(expression);
+Console.WriteLine("Массив из введенных чисел: ");
+OutPutArray(values);
+Console.WriteLine($"Количество положительных {numberPositive(values)}");
---
+++ Seminar6Task43/Program.cs
@@ -0,0 +1,47 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+// (Задание со звездочкой) Найдите площадь треугольника образованного 
+// пересечением 3 прямых
+
+
+Line[] inputLines()
+{   
+    int numLines = 3;
+    Line[] lines = new Line[0];
+    for(int i = 0; i < numLines; i++)
+    {
+        Line tempLine = new Line();
+        Console.WriteLine($"Input k{i}= ");
+        tempLine.k = double.Parse(Console.ReadLine()??"0");
+        Console.WriteLine($"Input b{i}= ");
+        tempLine.b = double.Parse(Console.ReadLine()??"0");
+        lines = lines.Append(tempLine).ToArray();
+
+    }
+    return lines;
+}
+
+bool Parallel(Line[] lines)
+{
+    for(int i = 0; i < lines.Length; i++)
+    {
+        for(int j = i+1; j < lines.Length; j++)
+        {
+            if(lines[i].k == lines[j].k)
+                return true;
+        }
+    }
+    return false;
+}
+
+Line[] lines = inputLines();
+Console.WriteLine(lines[1].k+" "+Parallel(lines));
+
+
+public class Line
+{
+    public double k;
+    public double b;
+}
